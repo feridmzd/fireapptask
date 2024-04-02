1157866bc7c9829f542f4dd840524ae850bcedc7
FeridBuludlu <buludluferid@gmail.com>
2024-04-02 09:16:10 +0400

    task added

---
+++ ConsoleAppPP/Fire.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPP
+{
+    public class Fire
+    {
+        public int CapacityBullet;
+        public int CurrentBullet;
+        public bool FireMode;
+        public bool Isstcock;
+        private bool firemode
+        {
+            get
+            {
+                return firemode;
+            }
+            set
+            {
+                if (!(Isstcock = true))
+                {
+
+                    Console.WriteLine("Tekli moda gecildi");
+                }
+                else
+                {
+                    Console.WriteLine("Avtomatik moda gecildi");
+                }
+
+            }
+
+        }
+        public Fire(int capacitybullet,int currentbullet,bool firemode)
+        {
+            CapacityBullet = capacitybullet;
+            CurrentBullet = currentbullet;
+            FireMode = firemode;
+        }
+
+        public void Shoot()
+        {
+            if (CurrentBullet == 0)
+            {
+                Reload();
+                Console.WriteLine("Daraq yeniden  doldururldu");
+            }
+            if(FireMode)
+            {
+                Console.WriteLine("");
+                CurrentBullet--;
+             }
+            else
+            {
+                Console.WriteLine("Tekli moda kecildi:"+CurrentBullet);
+                CurrentBullet= 0;
+            }
+        }
+       
+        public int GetremainBUllet()
+        {
+            return CapacityBullet-CurrentBullet;
+        }
+        public void Reload()
+        {
+            CurrentBullet=CapacityBullet;
+        }
+        public void ChangefIREMODE()
+        {
+            FireMode=!FireMode;
+            Console.WriteLine("Atis modu deyisildfi");
+        }
+    }
+}
---
+++ ConsoleAppPP/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace ConsoleAppPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fire fire = new Fire(25,17,true);
+            bool exit = false;
+            do
+            {
+                Console.WriteLine("Xos gelmissiniz\n");
+                Console.WriteLine("Zehmet olmasa reqemlerden birini secin");
+                Console.WriteLine("0:Infromasiya");
+                Console.WriteLine("1:Ates ac");
+                Console.WriteLine("2:Gulleelde et");
+                Console.WriteLine("3:Reload");
+                Console.WriteLine("4:Atesmodunu deyis");
+                Console.WriteLine("5:Programi dayandir");
+                char secim = Convert.ToChar(Console.ReadLine());
+                switch (secim)
+                {
+
+                    case '0':
+                        Console.WriteLine("Daragin gulle sayi:"+fire.CurrentBullet);
+                        Console.WriteLine("Daraqin gulle tutumu"+fire.CapacityBullet);
+                        break;
+
+
+                    case '1':
+                        fire.Shoot();
+                        break;
+
+
+                    case '2':
+                        Console.WriteLine("daraqda son gulle sayi"+fire.GetremainBUllet());
+                        break;
+
+                    case '3':
+                        fire.Reload();
+                        Console.WriteLine("Daraq yenien dolduruldu");
+                        break;
+
+
+
+                    case '4':
+                        fire.ChangefIREMODE();
+                        break;
+
+
+                    case '5':
+                        exit = true;
+                        break;
+
+
+                    default: 
+                        Console.WriteLine("Duzgun secim et");
+                        break;
+
+                }
+
+
+
+
+
+
+
+
+
+
+            }
+            while (!exit);
+
+        }
+    }
+}
